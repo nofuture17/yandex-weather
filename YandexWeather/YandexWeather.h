e89d8eaa5f944a29d3d80804803dc0ce080d4804
Suchilov Andrey <ar2041@bk.ru>
2020-09-05 12:55:53 +0300

    Initial Commit

---
+++ YandexWeather/YandexWeather.h
@@ -0,0 +1,19 @@
+//
+//  YandexWeather.h
+//  YandexWeather
+//
+//  Created by Анна on 05.09.2020.
+//  Copyright © 2020 ar2041@bk.ru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YandexWeather.
+FOUNDATION_EXPORT double YandexWeatherVersionNumber;
+
+//! Project version string for YandexWeather.
+FOUNDATION_EXPORT const unsigned char YandexWeatherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YandexWeather/PublicHeader.h>
+
+
